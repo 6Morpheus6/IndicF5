4a3bd802bf375de6a6608b3ed437a32208d34a7e
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2026-04-29 01:10:04 +0200

    bluefairy

--- torch.js
+++ torch.js
@@ -20,6 +20,7 @@ module.exports = {
       "when": "{{platform === 'win32' && gpu === 'amd'}}",
       "method": "shell.run",
       "params": {
+        "bluefairy": "off",
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": "uv pip install torch-directml torch torchaudio torchvision numpy==1.26.4 --force-reinstall"
@@ -53,6 +54,7 @@ module.exports = {
       "when": "{{platform === 'linux' && gpu === 'nvidia'}}",
       "method": "shell.run",
       "params": {
+        "bluefairy": "off",
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
